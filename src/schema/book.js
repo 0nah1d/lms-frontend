6104a6143aa9cce9c6ee0c8d30c4470b3d7f4723
Nahid <nahidthedeveloper@gmail.com>
2025-07-05 15:26:14 +0600

    change book schema

--- src/schema/book.js
+++ src/schema/book.js
@@ -3,31 +3,25 @@ import { z } from 'zod'
 export const bookSchema = z.object({
     title: z
         .string()
-        .min(5, 'Title must be at least 5 characters long')
-        .refine((val) => val.trim() !== '', 'Title is required'),
+        .min(1, 'Title is required')
+        .min(5, 'Title must be at least 5 characters long'),
     author: z
         .string()
-        .min(3, 'Author must be at least 3 characters long')
-        .refine((val) => val.trim() !== '', 'Author is required'),
+        .min(1, 'Author is required')
+        .min(3, 'Author must be at least 3 characters long'),
     genre: z
         .string()
-        .min(3, 'Genre must be at least 3 characters long')
-        .refine((val) => val.trim() !== '', 'Genre is required'),
+        .min(1, 'Genre is required')
+        .min(3, 'Genre must be at least 3 characters long'),
     image: z.any().refine((file) => file?.length === 1, 'Image is required'),
     description: z
         .string()
-        .min(10, 'Description must be at least 10 characters long')
-        .refine((val) => val.trim() !== '', 'Description is required'),
+        .min(1, 'Description is required')
+        .min(10, 'Description must be at least 10 characters long'),
     book_link: z
         .string()
-        .url('Book link must be a valid URL')
-        .refine((val) => val.trim() !== '', 'Book link is required'),
-    category: z
-        .string()
-        .min(1, 'Category is required')
-        .refine((val) => val.trim() !== '', 'Category is required'),
-    department: z
-        .string()
-        .min(1, 'Department is required')
-        .refine((val) => val.trim() !== '', 'Department is required'),
+        .min(1, 'Book link is required')
+        .url('Book link must be a valid URL'),
+    category: z.string().min(1, 'Category is required'),
+    department: z.string().min(1, 'Department is required'),
 })
